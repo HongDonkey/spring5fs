faf099be78cba10aa5daa0c8eb062d8dc290f27a
HongDonkey <hdk1992@naver.com>
2021-06-07 20:59:07 +0900

    210607

--- sp5-chap04/src/main/java/spring/MemberPrinter.java
+++ sp5-chap04/src/main/java/spring/MemberPrinter.java
@@ -29,9 +29,16 @@ public class MemberPrinter {
 	}
 	
 	@Autowired(required = false)
+	//자동 주입할 대상이 필수가 아닌 경우에는(required = false)로 지정
+	//(required = false)로 지정하면 빈이 없어도 익셉션이 발생하지 않으며 자동 주입을 수행하지 않음
 	public void setDateFormatter(DateTimeFormatter dateTimeFormatter) {
 		this.dateTimeFormatter = dateTimeFormatter;
 	}
+	
+//	@Autowired
+//	public void setDateFormatter(@Nullable DateTimeFormatter dateTimeFormatter) {
+//		this.dateTimeFormatter = dateTimeFormatter;
+//	}
 
 //	@Autowired
 //	public void setDateFormatter(Optional<DateTimeFormatter> formatterOpt) {
@@ -42,9 +49,6 @@ public class MemberPrinter {
 //		}
 //	}
 	
-//	@Autowired
-//	public void setDateFormatter(@Nullable DateTimeFormatter dateTimeFormatter) {
-//		this.dateTimeFormatter = dateTimeFormatter;
-//	}
+
 	
 }
